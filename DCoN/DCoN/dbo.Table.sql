4f00e9e11bb9a5bb131ebfe68eb3eecdff4a3440
ReshmaRamesh12 <reshmaramesh1209@gmail.com>
2017-05-19 15:52:39 -0700

    data db creation

---
+++ DCoN/DCoN/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Username] VARCHAR(50) NULL, 
+    [FilleName] VARCHAR(50) NULL, 
+    [uploaddatetime] DATETIME NULL, 
+    [convertdatetime] DATETIME NULL, 
+    [downloaddatetime] DATETIME NULL
+)
